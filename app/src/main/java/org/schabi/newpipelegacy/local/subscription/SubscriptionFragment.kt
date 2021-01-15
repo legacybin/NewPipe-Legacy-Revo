6f5cb622a2549042b5e97fd165a9b6bf32cc509e
Stypox <stypox@pm.me>
2021-01-15 08:15:09 +0100

    Merge pull request TeamNewPipe/NewPipe#5417 from Isira-Seneviratne/Fix_crash_on_back_navigation
    Fix the crash that occurs on navigating back to the main fragment.

--- app/src/main/java/org/schabi/newpipelegacy/local/subscription/SubscriptionFragment.kt
+++ app/src/main/java/org/schabi/newpipelegacy/local/subscription/SubscriptionFragment.kt
@@ -313,10 +313,10 @@ class SubscriptionFragment : BaseStateFragment<SubscriptionState>() {
         dialogTitleBinding.itemAdditionalDetails.visibility = View.GONE
 
         AlertDialog.Builder(requireContext())
-                .setCustomTitle(dialogTitleBinding.root)
-                .setItems(commands, actions)
-                .create()
-                .show()
+            .setCustomTitle(dialogTitleBinding.root)
+            .setItems(commands, actions)
+            .create()
+            .show()
     }
 
     private fun deleteChannel(selectedItem: ChannelInfoItem) {
