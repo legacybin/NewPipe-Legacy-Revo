3a813e459e3c8bc672ffd6df97044f7901d729bf
ShareASmile <60492161+ShareASmile@users.noreply.github.com>
2024-07-12 19:40:46 +0530

    some visual changes for better visual experience
    1.  Video Title in Description now shows 2 lines
    2.  Size of description text under videos increased to 14sp for better visual experience
    3. Size of comments in comments section increased to 14sp
    4. a visual fixes in background player activity

--- app/src/main/java/org/schabi/newpipelegacy/fragments/detail/VideoDetailFragment.java
+++ app/src/main/java/org/schabi/newpipelegacy/fragments/detail/VideoDetailFragment.java
@@ -495,7 +495,7 @@ public class VideoDetailFragment extends BaseStateFragment<StreamInfo>
 
     private void toggleTitleAndDescription() {
         if (videoDescriptionRootLayout.getVisibility() == View.VISIBLE) {
-            videoTitleTextView.setMaxLines(1);
+            videoTitleTextView.setMaxLines(2);
             videoDescriptionRootLayout.setVisibility(View.GONE);
             videoDescriptionView.setFocusable(false);
             videoTitleToggleArrow.setImageResource(
@@ -1144,7 +1144,7 @@ public class VideoDetailFragment extends BaseStateFragment<StreamInfo>
         animateView(positionView, false, 50);
 
         videoTitleTextView.setText(name != null ? name : "");
-        videoTitleTextView.setMaxLines(1);
+        videoTitleTextView.setMaxLines(2);
         animateView(videoTitleTextView, true, 0);
 
         videoDescriptionRootLayout.setVisibility(View.GONE);
